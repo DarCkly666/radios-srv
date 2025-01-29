06274c8caebe94ecc356fe56ed2176710492f01f
DarCkly6 <darckly6@gmail.com>
2025-01-28 20:57:40 -0400

    First commit

---
+++ src/main/java/com/darckly/radiosapi/RadiosapiApplication.java
@@ -0,0 +1,13 @@
+package com.darckly.radiosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadiosapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadiosapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darckly/radiosapi/RadiosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darckly.radiosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadiosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
